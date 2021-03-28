6621d44a323fb78f13bf79c7df57b653c8c9e2d6
Zef Hemel <zef@zef.me>
2021-03-28 10:35:50 +0200

    Initial checkin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from "react";
+
+import Editor from "@monaco-editor/react";
+import monaco from "monaco-editor";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const editorRef: React.MutableRefObject<monaco.editor.IStandaloneCodeEditor | null> = useRef(null);
+
+    function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
+        editorRef.current = editor;
+    }
+
+    function showValue() {
+        if (editorRef.current !== null) {
+            alert(editorRef.current.getValue());
+        }
+    }
+
+    return (
+        <>
+            <button onClick={showValue}>Show value</button>
+            <Editor
+                height="90vh"
+                defaultLanguage="markdown"
+                defaultValue="# Some header"
+                options={{
+                    minimap: {
+                        enabled: false
+                    },
+                }}
+                onMount={handleEditorDidMount}
+            />
+        </>
+    );
 }
+
 
 export default App;
